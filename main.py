a0e624a2ef7a50bf93efe04d3f0875b6434e011d
KToToTaM <45483746+KToToTaM@users.noreply.github.com>
2023-04-16 14:40:08 +0300

    Update main.py
    tryna add progress bars + minprice fix

--- main.py
+++ main.py
@@ -57,19 +57,24 @@ prices = { 'set':[],
            'return':[]
            }
          
-###req = json.loads(requests.get("https://api.warframe.market/v1/items/mirage_prime_systems/orders").text)
-###i need to check parts, somehow check the highest one
-
-
+###pulling stuff from warframe market, if fails it retries
 def wfmget(part_suffix, warframeID, try_number=1):
     try:
-        rWFMget = requests.get(f"https://api.warframe.market/v1/items/{warframe[warframeID]}_prime_{part_suffix}/orders").json()
+        rWFMget = requests.get(f"https://api.warframe.market/v1/items/{warframe[warframeID]}_prime_{part_suffix}/orders").json()  ##https://warframe.market/api_docs
     except (requests.exceptions.ConnectionError, json.decoder.JSONDecodeError):
         sleep(2**try_number + random()*0.01) #exponential backoff
         return wfmget(part_suffix, warframeID, try_number=try_number+1)
     else:
         return rWFMget['payload']['orders']
 
+def progress_bar(currentProgress, part_suffix, total=37):
+    global warframe
+    percentage = 100 * (currentProgress / float(total))
+    bar = '█' * int(percentage) + '-' * (100 - int(percentage))
+    
+    
+    print(f"Checking {part_suffix}(s)...     Current warframe: {warframe[currentProgress].title()} Prime\n\r|{bar}| {currentProgress}\{total}\r")
+    sys.stdout.write("\033[F")
 ###im gonna need to do this shit ..4 times so im gonna yeet the entire thing into a func and pass the params that change.
 def chkprice(part_suffix):
     
@@ -77,8 +82,10 @@ def chkprice(part_suffix):
     global warframe
     global warframealtsuffix
     
-    print(f"###{part_suffix} start###")
+    
     for warframeID in range(0,len(warframe)):
+        
+        progress_bar(warframeID,part_suffix)
         
         ###patch , newer warframes have an extra "_blueprint" for some reason
         for warframealtID in range(0,len(warframealtsuffix)):
@@ -87,20 +94,15 @@ def chkprice(part_suffix):
         
         
         rWFMget = wfmget(part_suffix,warframeID)
-        ##filtering out useless orders , api puts everything in one call
-        ##while loop cause for some reason the for loop doesnt bloody work man
-        orderID = 0
-        while orderID <= len(rWFMget)-1:
-            if rWFMget[orderID]["order_type"] != "sell" or rWFMget[orderID]["user"]["status"] != 'ingame' or rWFMget[orderID]["visible"] != 'true' :
-                rWFMget.pop(orderID)
-            orderID += 1   
+
+        ###filtering out what we need, and throwing away the unneeded
+        minprice = []
+        for orderID in range(0,len(rWFMget)):
+            if rWFMget[orderID]['order_type'] == 'sell' and rWFMget[orderID]['user']['status'] == 'ingame':
+                minprice.append(rWFMget[orderID]['platinum'])
                 
         
-        ##finding the min part
-        for numberID in range(0,len(rWFMget)):
-            minprice = []
-            minprice.append(rWFMget[numberID]['platinum'])
-            minprice = min(minprice)
+        minprice = min(minprice)
         
         ###another patch because whooooops , apparently adding _blueprint results in a KeyError :clueless:
         
@@ -108,8 +110,8 @@ def chkprice(part_suffix):
                 part_suffix = part_suffix[:-10] ### if we have _blueprint , we remove em 
                 
         prices[part_suffix].append(minprice)
-        print(f"{warframe[warframeID]} done")
-    print(f"###{part_suffix} end###")
+        
+    
 ###actually getting the prices
 
 chkprice('set')
@@ -138,3 +140,5 @@ wfmtable.field_names = ["warframe","blueprint","systems","chassis","neuroptics",
 for warframeID in range(0,len(warframe)):
     wfmtable.add_row([warframe[warframeID],prices['blueprint'][warframeID],prices['systems'][warframeID],prices['chassis'][warframeID],prices['neuroptics'][warframeID],prices['set'][warframeID],prices['investment'][warframeID],prices['return'][warframeID]])
 print(wfmtable)
+
+dont_close_blyat = input("press anythin to close mate")
